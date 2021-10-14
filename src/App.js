179c5c5018267b6b7a7d250527d60708f0a8b8d0
girts521 <girts521@gmail.com>
2021-10-14 07:06:18 +0200

    This is a test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hell yeeee running from docker
+           really really running from docker
         </p>
         <a
           className="App-link"
